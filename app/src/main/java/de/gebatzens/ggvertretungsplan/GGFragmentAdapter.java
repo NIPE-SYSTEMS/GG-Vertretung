d05de971cd4793f9f740a5fd78dab46bdbf757e9
gebatzens <gebatzens@gebatzens.de>
2014-12-11 17:27:07 +0100

    first commit

---
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/GGFragmentAdapter.java
@@ -0,0 +1,87 @@
+/*
+ * This file is part of GGVertretungsplan.
+ *
+ * GGVertretungsplan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GGVertretungsplan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GGVertretungsplan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This file is part of GGVertretungsplan.
+ *
+ * GGVertretungsplan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GGVertretungsplan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GGVertretungsplan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.gebatzens.ggvertretungsplan;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by hauke on 10.12.14.
+ */
+public class GGFragmentAdapter extends FragmentPagerAdapter {
+
+    Fragment heute, morgen, overview;
+
+    public GGFragmentAdapter(FragmentManager m) {
+        super(m);
+        heute = new GGFragment();
+        morgen = new GGFragment();
+        overview = new GGFragment();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+            case 0:
+                return overview;
+            case 1:
+                return heute;
+            case 2:
+                return morgen;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int p) {
+        switch(p) {
+            case 0:
+                return "asdersicht";
+            case 1:
+                return "Heute";
+            case 2:
+                return "Morgen";
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
---
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/MainActivity.java
@@ -0,0 +1,98 @@
+/*
+ * This file is part of GGVertretungsplan.
+ *
+ * GGVertretungsplan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GGVertretungsplan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GGVertretungsplan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This file is part of GGVertretungsplan.
+ *
+ * GGVertretungsplan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GGVertretungsplan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GGVertretungsplan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.gebatzens.ggvertretungsplan;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends FragmentActivity {
+
+    GGFragmentAdapter mAdapter;
+    ViewPager mPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdapter = new GGFragmentAdapter(this.getSupportFragmentManager());
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+
+                }
+
+                return false;
+            }
+        });
+
+        toolbar.setTitle("Vertretung");
+        toolbar.inflateMenu(R.menu.toolbar_menu);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+
+    }
+
+
+
+}
