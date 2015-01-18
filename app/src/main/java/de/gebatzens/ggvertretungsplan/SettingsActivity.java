a6c7ddb5287aa4bb251a30e8086573766b0e6b6c
LRAnimations <lranimations@gmail.com>
2015-01-18 15:35:46 +0100

    Improve

--- app/src/main/java/de/gebatzens/ggvertretungsplan/SettingsActivity.java
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/SettingsActivity.java
@@ -19,14 +19,41 @@ package de.gebatzens.ggvertretungsplan;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class SettingsActivity extends PreferenceActivity {
 
+    private Toolbar mToolBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.preferences);
+
+        mToolBar.setTitle(getTitle());
     }
 
+    @Override
+    public void setContentView(int layoutResID) {
+        ViewGroup contentView = (ViewGroup) LayoutInflater.from(this).inflate(
+                R.layout.settings_activity, new LinearLayout(this), false);
+
+        mToolBar = (Toolbar) contentView.findViewById(R.id.toolbar);
+        mToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        ViewGroup contentWrapper = (ViewGroup) contentView.findViewById(R.id.content_wrapper);
+        LayoutInflater.from(this).inflate(layoutResID, contentWrapper, true);
+
+        getWindow().setContentView(contentView);
+    }
 }
