57fccca9d9b0da79519a9b4d1bff07631a67b803
Gebatzens <gebatzens@gebatzens.de>
2015-01-19 21:16:55 +0100

    Add missing class...

---
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/GGInvalidSourceException.java
@@ -0,0 +1,33 @@
+/*
+ * This file is part of GGVertretungsplan.
+ *
+ * GGVertretungsplan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GGVertretungsplan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GGVertretungsplan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.gebatzens.ggvertretungsplan;
+
+public class GGInvalidSourceException extends Exception {
+
+    String extra;
+
+    public GGInvalidSourceException(String extra) {
+        this.extra = extra;
+    }
+
+    @Override
+    public String getMessage() {
+        return extra == null ? "The received HTML is invalid." : "The received HTML is invalid: " + extra;
+    }
+
+}
