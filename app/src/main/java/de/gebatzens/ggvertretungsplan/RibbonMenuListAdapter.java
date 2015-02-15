61baaadcd6f553488ac74b7da0d1f81031fec38b
asterix11 <fabian@microbotik.de>
2015-02-15 20:15:22 +0100

    Changed RibbonMenuListAdpater.

--- app/src/main/java/de/gebatzens/ggvertretungsplan/RibbonMenuListAdapter.java
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/RibbonMenuListAdapter.java
@@ -1,7 +1,20 @@
-/* Copyright (C) Fabian Schultis - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Fabian Schultis <fabian@microbotik.de>, May 2014
+/*
+ * Copyright (C) 2015 Hauke Oldsen
+ *
+ * This file is part of GGVertretungsplan.
+ *
+ * GGVertretungsplan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GGVertretungsplan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GGVertretungsplan.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.gebatzens.ggvertretungsplan;
