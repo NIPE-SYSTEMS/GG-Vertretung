ef88f5f16ddce7127e5478b3e5def9f6279393d5
asterix11 <fabian@microbotik.de>
2015-02-24 12:22:53 +0100

    Fixed accidentaly added letter.

--- app/src/main/java/de/gebatzens/ggvertretungsplan/NewsFragmentDatabaseHelper.java
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/NewsFragmentDatabaseHelper.java
@@ -1,22 +1,21 @@
 /*
-* Copyright (C) 2015 Fabian Schultis
-*
-* This file is part of GGVertretungsplan.
-*
-* GGVertretungsplan is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-*
-* GGVertretungsplan is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with GGVertretungsplan. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2015 Fabian Schultis
+ *
+ * This file is part of GGVertretungsplan.
+ *
+ * GGVertretungsplan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GGVertretungsplan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GGVertretungsplan.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package de.gebatzens.ggvertretungsplan;
 
@@ -43,7 +42,7 @@ public class NewsFragmentDatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db)h {
+    public void onCreate(SQLiteDatabase db) {
         db.execSQL(NEWS_TABLE_CREATE);
     }
 
