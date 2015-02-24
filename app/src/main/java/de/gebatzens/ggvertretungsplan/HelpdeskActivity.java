a8e15de6bc7a9b6256c9196cf63167dafbb1b10e
LRAnimations <lranimations@gmail.com>
2015-02-24 19:12:16 +0100

    Redesign helpdesk activity

--- app/src/main/java/de/gebatzens/ggvertretungsplan/HelpdeskActivity.java
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/HelpdeskActivity.java
@@ -26,10 +26,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,9 +35,7 @@ import javax.net.ssl.HttpsURLConnection;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.IOError;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -76,85 +72,87 @@ public class HelpdeskActivity extends Activity {
         final TextView mTextViewSubject = (TextView) findViewById(R.id.reportSubject);
         final TextView mTextViewMessage = (TextView) findViewById(R.id.reportMessage);
 
-        Button mButtonSubmit = (Button) findViewById(R.id.reportSubmit);
-        mButtonSubmit.setOnClickListener(new View.OnClickListener() {
+        mToolBar.inflateMenu(R.menu.helpdesk_menu);
+        mToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-            public void onClick(View v) {
-                String name = mTextViewName.getText().toString();
-                String email = mTextViewEmail.getText().toString();
-                String subject = mTextViewSubject.getText().toString();
-                String message = mTextViewMessage.getText().toString();
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                        String name = mTextViewName.getText().toString();
+                        String email = mTextViewEmail.getText().toString();
+                        String subject = mTextViewSubject.getText().toString();
+                        String message = mTextViewMessage.getText().toString();
 
-                new AsyncTask<String, Integer, Integer>() {
-                    @Override
-                    protected Integer doInBackground(String... params) {
-                        try {
-                            if((params[0]!=null)&&(params[1]!=null)&&(params[2]!=null)&&(params[3]!=null)&&!params[0].equals("")&&!params[1].equals("")&&!params[2].equals("")&&!params[3].equals("")) {
-                                HttpsURLConnection con = (HttpsURLConnection) new URL("https://gymnasium-glinde.logoip.de/infoapp/infoapp_helpdesk.php").openConnection();
+                        new AsyncTask<String, Integer, Integer>() {
+                            @Override
+                            protected Integer doInBackground(String... params) {
+                                try {
+                                    if ((params[0] != null) && (params[1] != null) && (params[2] != null) && (params[3] != null) && !params[0].equals("") && !params[1].equals("") && !params[2].equals("") && !params[3].equals("")) {
+                                        HttpsURLConnection con = (HttpsURLConnection) new URL("https://gymnasium-glinde.logoip.de/infoapp/infoapp_helpdesk.php").openConnection();
 
-                                con.setRequestMethod("POST");
-                                con.setSSLSocketFactory(GGProvider.sslSocketFactory);
+                                        con.setRequestMethod("POST");
+                                        con.setSSLSocketFactory(GGProvider.sslSocketFactory);
 
-                                con.setDoOutput(true);
-                                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-                                String urlParams;
-                                if(!GGApp.GG_APP.provider.getUsername().equals("")) {
-                                    urlParams = "name=" + URLEncoder.encode(params[0],"UTF-8") + "&email=" + URLEncoder.encode(params[1],"UTF-8") + "&subject=" + URLEncoder.encode(params[2],"UTF-8") + "&message=" + URLEncoder.encode(params[3],"UTF-8") + "&username=" + URLEncoder.encode(GGApp.GG_APP.provider.getUsername(),"UTF-8");
-                                } else {
-                                    urlParams = "name=" + URLEncoder.encode(params[0],"UTF-8") + "&email=" + URLEncoder.encode(params[1],"UTF-8") + "&subject=" + URLEncoder.encode(params[2],"UTF-8") + "&message=" + URLEncoder.encode(params[3],"UTF-8");
-                                }
-                                Log.d("urlParams", urlParams);
-                                wr.writeBytes(urlParams);
-                                wr.flush ();
-                                wr.close ();
+                                        con.setDoOutput(true);
+                                        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                                        String urlParams;
+                                        if (!GGApp.GG_APP.provider.getUsername().equals("")) {
+                                            urlParams = "name=" + URLEncoder.encode(params[0], "UTF-8") + "&email=" + URLEncoder.encode(params[1], "UTF-8") + "&subject=" + URLEncoder.encode(params[2], "UTF-8") + "&message=" + URLEncoder.encode(params[3], "UTF-8") + "&username=" + URLEncoder.encode(GGApp.GG_APP.provider.getUsername(), "UTF-8");
+                                        } else {
+                                            urlParams = "name=" + URLEncoder.encode(params[0], "UTF-8") + "&email=" + URLEncoder.encode(params[1], "UTF-8") + "&subject=" + URLEncoder.encode(params[2], "UTF-8") + "&message=" + URLEncoder.encode(params[3], "UTF-8");
+                                        }
+                                        Log.d("urlParams", urlParams);
+                                        wr.writeBytes(urlParams);
+                                        wr.flush();
+                                        wr.close();
 
-                                if(con.getResponseCode()==200) {
-                                    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                                    String line;
-                                    StringBuilder sb = new StringBuilder();
-                                    while((line = br.readLine()) != null) {
-                                        sb.append(line);
-                                    }
-                                    br.close();
-                                    if(sb.toString().contains("<state>true</state>")) {
-                                        return 0;
+                                        if (con.getResponseCode() == 200) {
+                                            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                                            String line;
+                                            StringBuilder sb = new StringBuilder();
+                                            while ((line = br.readLine()) != null) {
+                                                sb.append(line);
+                                            }
+                                            br.close();
+                                            if (sb.toString().contains("<state>true</state>")) {
+                                                return 0;
+                                            } else {
+                                                return 2;
+                                            }
+                                        } else {
+                                            return 2;
+                                        }
+
                                     } else {
-                                        return 2;
+                                        return 1;
                                     }
-                                } else {
+                                } catch (MalformedURLException e) {
+                                    e.printStackTrace();
+                                    return 2;
+                                } catch (IOException e) {
+                                    e.printStackTrace();
                                     return 2;
                                 }
-
-                            } else {
-                                return 1;
                             }
-                        } catch(MalformedURLException e) {
-                            e.printStackTrace();
-                            return 2;
-                        } catch(IOException e) {
-                            e.printStackTrace();
-                            return 2;
-                        }
-                    }
 
-                    @Override
-                    protected void onPostExecute(Integer result) {
-                        if(result==0) {
-                            Toast.makeText(getApplicationContext(),getResources().getString(R.string.message_sent_successfully),Toast.LENGTH_LONG).show();
-                            finish();
-                        } else if(result==1) {
-                            Toast.makeText(getApplicationContext(),getResources().getString(R.string.please_fill_out_all_inputs),Toast.LENGTH_LONG).show();
-                        } else if(result==2) {
-                            Toast.makeText(getApplicationContext(),getResources().getString(R.string.error_while_sending_message),Toast.LENGTH_LONG).show();
-                        }
-                    }
-                }.execute(name,email,subject,message);
+                            @Override
+                            protected void onPostExecute(Integer result) {
+                                if (result == 0) {
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.message_sent_successfully), Toast.LENGTH_LONG).show();
+                                    finish();
+                                } else if (result == 1) {
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.please_fill_out_all_inputs), Toast.LENGTH_LONG).show();
+                                } else if (result == 2) {
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.error_while_sending_message), Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }.execute(name, email, subject, message);
+                return false;
             }
-        });
-    }
 
-    @Override
-    public void onBackPressed() {
-        finish();
-    }
-}
+            });
+        }
+
+            @Override
+            public void onBackPressed() {
+                finish();
+            }
+        }
