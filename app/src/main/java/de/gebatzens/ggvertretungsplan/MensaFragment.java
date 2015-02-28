04d7dfbb00d346d69eeda34fc7edd056f366908c
asterix11 <fabian@microbotik.de>
2015-02-28 23:45:06 +0100

    Last changes on the MensaFragment and release preparations for 0.9.4.

--- app/src/main/java/de/gebatzens/ggvertretungsplan/MensaFragment.java
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/MensaFragment.java
@@ -30,6 +30,7 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.CardView;
 import android.text.Html;
@@ -47,6 +48,9 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -67,6 +71,7 @@ public class MensaFragment extends RemoteDataFragment {
 
     SwipeRefreshLayout swipeContainer;
     int cardColorIndex = 0;
+    String cache_file_prefix = "cache_mensa_";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle bundle) {
@@ -213,7 +218,21 @@ public class MensaFragment extends RemoteDataFragment {
             protected ViewHolder doInBackground(ViewHolder... params) {
                 //params[0].bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.block_house_steak);
                 try {
-                    params[0].bitmap = GGApp.GG_APP.provider.getMensaImage(params[0].filename);
+                    Bitmap bitmap;
+                    if(cacheCheckDir()) {
+                        bitmap = cacheGetBitmap(params[0].filename);
+                        if(bitmap!=null) {
+                            params[0].bitmap = bitmap;
+                        } else {
+                            bitmap = GGApp.GG_APP.provider.getMensaImage(params[0].filename);
+                            cacheSetBitmap(params[0].filename, bitmap);
+                            params[0].bitmap = bitmap;
+                        }
+                    } else {
+                        bitmap = GGApp.GG_APP.provider.getMensaImage(params[0].filename);
+                        cacheSetBitmap(params[0].filename, bitmap);
+                        params[0].bitmap = bitmap;
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                     params[0].bitmap = null;
@@ -285,6 +304,45 @@ public class MensaFragment extends RemoteDataFragment {
         {
             e.printStackTrace();
             return "";
+        }
+    }
+
+    private boolean cacheCheckDir() {
+        File schulinfoapp_dir = new File(Environment.getExternalStorageDirectory() + "/SchulinfoAPP");
+        if(schulinfoapp_dir.isDirectory()) {
+            return true;
+        } else {
+            schulinfoapp_dir.mkdirs();
+            if(schulinfoapp_dir.isDirectory()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public Bitmap cacheGetBitmap(String filename) {
+        File bitmap_file = new File(Environment.getExternalStorageDirectory() + "/SchulinfoAPP/" + cache_file_prefix + filename);
+        if(bitmap_file.exists()) {
+            return BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/SchulinfoAPP/" + cache_file_prefix + filename);
+        } else {
+            return null;
+        }
+    }
+
+    private void cacheSetBitmap(String filename, Bitmap image) {
+        File bitmap_file = new File(Environment.getExternalStorageDirectory() + "/SchulinfoAPP/" + cache_file_prefix + filename);
+        try {
+            if(!bitmap_file.exists()) {
+                bitmap_file.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(bitmap_file);
+            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
