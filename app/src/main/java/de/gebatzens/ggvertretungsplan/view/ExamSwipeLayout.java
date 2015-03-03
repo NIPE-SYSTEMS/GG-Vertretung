16f790417aec0127878b94022d2b1d54c923440f
asterix11 <fabian@microbotik.de>
2015-03-03 18:34:11 +0100

    Added missing file.

---
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/view/ExamSwipeLayout.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015 Hauke Oldsen
+ *
+ * This file is part of GGVertretungsplan.
+ *
+ * GGVertretungsplan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GGVertretungsplan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GGVertretungsplan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.gebatzens.ggvertretungsplan.view;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+import de.gebatzens.ggvertretungsplan.GGApp;
+import de.gebatzens.ggvertretungsplan.MainActivity;
+
+public class ExamSwipeLayout extends SwipeRefreshLayout {
+
+    public ExamSwipeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+
+                if(GGApp.GG_APP.getFragmentType() == GGApp.FragmentType.EXAMS) {
+                    ScrollView sv = ((ScrollView) ((MainActivity) getContext()).mContent.getView().findViewWithTag("exam_scroll"));
+
+                    if(sv != null) {
+                        int i = -sv.getScrollY();
+
+                        if (i != 0)
+                            return false;
+                    } else
+                        return super.onInterceptTouchEvent(event);
+                }
+        }
+
+        return super.onInterceptTouchEvent(event);
+    }
+}
